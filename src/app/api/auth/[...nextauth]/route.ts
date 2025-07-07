4576b6a36b8815dff99d151b3ffb42a06ae1e9e1
jubayer718 <jubayerahmed2462@gmail.com>
2025-07-07 16:05:51 +0600

    deploy.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// import NextAuth from 'next-auth';
-// import GoogleProvider from 'next-auth/providers/google';
-// import { NextAuthOptions } from 'next-auth';
-
-
-// export const authOption: NextAuthOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-
-//   pages: {
-//     signIn: '/',
-//   },
-//   callbacks: {
-//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//     async redirect({ url, baseUrl }) {
-//       return '/agents'
-//     },
-//   },
-// };
-
-// const handler = NextAuth(authOption);
-// export { handler as GET, handler as POST };
-
-
-
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -38,10 +9,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  pages: {
-    signIn:'/'
-  }
-  ,
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async redirect({ url, baseUrl }) {
       return '/agents';
